803bd7cc7102286191f729b470a3853267dd8830
Henrik L <henrik.lowenberg@gmail.com>
2023-09-04 17:43:06 +0200

    --amend

--- test_roles.py
+++ test_roles.py
@@ -7,44 +7,56 @@ import json
 import tempfile
 import shutil
 
-sshpass = getpass.getpass('Enter SSH password: ')
-pwd_dict: dict = { r'SSH [pP]assword:\s*$': sshpass }
-hosts = {
+secret = getpass.getpass('Enter SSH password: ')
+sshpass: dict = { r'SSH [pP]assword:\s*$': secret }
+hosts: dict = {
     "local": {
         "hosts": {
+            #"dummyhost.some.domain": {},
             "localhost": {
                 'ansible_connection': 'local'
 			},
         },
+        'vars': {
+	        'ansible_ssh_common_args': '-o StrictHostKeyChecking=no',
+	        'ansible_user': 'root',
+	        'ansible_timeout': 10,
+            'gather_facts': False,
+        }
     }
 }
 
-envvars = {
-	'ansible_ssh_common_args': '-o StrictHostKeyChecking=no',
-	'ansible_user': 'root',
-	'ansible_timeout': 10,
+path = '/some/obscure/dir:'
+path += os.environ.get('PATH', '')
+envvars: dict = {
+	'PATH': path,
+    'PYTHONPATH': '/opt/homebrew/bin/python3:',
+    'ANSIBLE_ROLES_PATH': 'etc/ansible/roles:'
 }
-
 temp_datadir = tempfile.mkdtemp()
 saved_umask = os.umask(0o0077)
+role_name = "test_role"
+role_vars: dict = {'var1': "value1"}
 
 kwargs = {
 	'private_data_dir': temp_datadir,
 	'roles_path': os.getcwd() + "/roles",
-	'role': "test_role",
-	'role_vars': {'var1': "value1"},
+	'role': role_name,
+	'role_vars': role_vars,
 	'passwords': sshpass,
 	'inventory': hosts,
-	'envvars': envvars,
-	'verbosity': 5,
+	#'envvars': envvars,
+	'cmdline': '--ask-pass',
+	'verbosity': 2,
 	'suppress_env_files':True,
 }
 
-r_result = ansible_runner.interface.run(**kwargs)
-stdout = r_result.stdout.read()
-events = list(r_result.events)
-stats = r_result.stats
-print(json.dumps(stats, indent=4))
-
-os.umask(saved_umask)
-shutil.rmtree(temp_datadir)
+try:
+  r_result = ansible_runner.interface.run(**kwargs)
+  stdout = r_result.stdout.read()
+  events = list(r_result.events)
+  stats = r_result.stats
+  print(json.dumps(stats, indent=4))
+finally:
+  os.umask(saved_umask)
+  shutil.rmtree(temp_datadir)
